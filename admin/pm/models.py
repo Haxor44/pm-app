913110afd844a362df370d8876b98b35c9ce352e
Haxor44 <evolmalek04@gmail.com>
2023-11-26 18:42:53 +0300

    updates

--- admin/pm/models.py
+++ admin/pm/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 class Pmforms(models.Model):
     name = models.CharField(max_length=200)
-    staff_id = models.IntegerField()
-    serial_number = models.IntegerField()
+    staff_id = models.CharField(max_length=70,default="")
+    serial_number = models.CharField(max_length=70)
     dept_name = models.CharField(max_length=200)
+    device_name = models.CharField(max_length=200,default="")
 
 class USer(models.Model):
     pass
